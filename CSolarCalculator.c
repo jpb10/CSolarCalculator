ab795aa738a92b0fcaf19598f634bd59964a7b70
jpb10 <4648556+jpb10@users.noreply.github.com>
2025-05-19 11:22:19 +0200

    Update CSolarCalculator

--- CSolarCalculator.c
+++ CSolarCalculator.c
@@ -1,11 +1,13 @@
-//======================================================================================================================
-// SolarCalculator Library for Arduino (C version)
-//
-// This library provides functions to calculate the times of sunrise, sunset, solar noon, twilight (dawn and dusk),
-// Sun's apparent position in the sky, equation of time, etc.
-//
-// Most formulae are taken from Astronomical Algorithms by Jean Meeus and optimized for 8-bit AVR platform.
-//======================================================================================================================
+/*
+ * SolarCalculator Library for Arduino (C version)
+ *
+ * This library provides functions to calculate the times of sunrise, sunset,
+ * solar noon, twilight (dawn and dusk), Sun's apparent position in the sky,
+ * equation of time, etc.
+ *
+ * Most formulae are taken from Astronomical Algorithms by Jean Meeus and
+ * optimized for Arduino.
+ */
 
 #ifdef ARDUINO
 #include <Arduino.h>
@@ -15,42 +17,56 @@
 
 #include "CSolarCalculator.h"
 
-const double SUNRISESET_STD_ALTITUDE = -0.8333;
-const double CIVIL_DAWNDUSK_STD_ALTITUDE = -6.0;
-const double NAUTICAL_DAWNDUSK_STD_ALTITUDE = -12.0;
-const double ASTRONOMICAL_DAWNDUSK_STD_ALTITUDE = -18.0;
+/*
+ * Get Julian Day, given:
+ *
+ *  - Unix timestamp, elapsed seconds since 1970 Jan 01, 00:00:00 UTC, e.g.
+ *      ts      946684800       2000 Jan 01, 00:00:00 UTC
+ *
+ *  - Calendar date and time (UTC), with valid ranges:
+ *      year    [1901..2099]
+ *      month   [1..12]
+ *      day     [1..31]
+ *      hour    [0..23]
+ *      minute  [0..59]
+ *      second  [0..60]
+ */
 
-struct JulianDay jdFromUnix(unsigned long utc)
+JulianDay jdFromUnix(unsigned long unix_ts)
 {
-    struct JulianDay jd;
-    jd.JD = (unsigned long)(utc / 86400) + 2440587.5;
-    jd.m = (utc % 86400) / 86400.0;
+    JulianDay jd;
+    jd.JD = (unsigned long)(unix_ts / 86400) + 2440587.5;
+    jd.m = (unix_ts % 86400) / 86400.0;
     return jd;
 }
 
-struct JulianDay jdFromDate(int year, int month, int day, int hour, int minute, int second)
+JulianDay jdFromDate(int year, int month, int day,
+                     int hour, int minute, int second)
 {
-    struct JulianDay jd;
-    jd.JD = calcJulianDay(year, month, day);
-    jd.m = fractionalDay(hour, minute, second);
+    JulianDay jd;
+    /* Valid from 1901 to 2099 (Van Flandern & Pulkkinen, 1979) */
+    jd.JD = 367.0 * year - (int)(7 * (year + (month + 9) / 12) / 4) +
+            (int)(275 * month / 9) + day + 1721013.5;
+    jd.m = (hour + minute / 60.0 + second / 3600.0) / 24.0;
     return jd;
 }
 
-//======================================================================================================================
-// Intermediate calculations
-//
-// Time T is measured in Julian centuries (36525 ephemeris days from the epoch J2000.0)
-//======================================================================================================================
+/*
+ * Intermediate calculations
+ *
+ * NB. Time T is measured in Julian centuries
+ *     (36525 ephemeris days from the epoch J2000.0)
+ */
 
 #ifndef ARDUINO
 double radians(double deg)
 {
-    return deg * M_PI / 180;
+    return deg * 0.017453292519943296;
 }
 
 double degrees(double rad)
 {
-    return rad * 180 / M_PI;
+    return rad * 57.295779513082321;
 }
 #endif
 
@@ -58,99 +74,107 @@ double wrapTo360(double angle)
 {
     angle = fmod(angle, 360);
     if (angle < 0) angle += 360;
-    return angle;  // [0, 360)
+    return angle; /* [0, 360) */
 }
 
 double wrapTo180(double angle)
 {
     angle = wrapTo360(angle + 180);
-    return angle - 180;  // [-180, 180)
+    return angle - 180; /* [-180, 180) */
 }
 
-double fractionalDay(int hour, int minute, int second)
+double calcJulianCent(JulianDay jd)
 {
-    return (hour + minute / 60.0 + second / 3600.0) / 24;
-}
-
-// Valid from 1901 to 2099, Van Flandern & Pulkkinen (1979)
-double calcJulianDay(int year, int month, int day)
-{
-    return 367.0 * year - (int)(7 * (year + (month + 9) / 12) / 4) + (int)(275 * month / 9) + day + 1721013.5;
-}
-
-double calcJulianCent(struct JulianDay jd)
-{
-    return ((jd.JD - 2451545) + jd.m) / 36525;
+    return (jd.JD - 2451545 + jd.m) / 36525;
 }
 
 double calcGeomMeanLongSun(double T)
 {
-    return wrapTo360(280.46646 + T * 36000.76983);  // in degrees
+    return wrapTo360(280.46646 + T * 36000.76983); /* in degrees */
 }
 
 double calcGeomMeanAnomalySun(double T)
 {
-    return wrapTo360(357.52911 + T * 35999.05029);  // in degrees
+    return wrapTo360(357.52911 + T * 35999.05029); /* in degrees */
 }
 
 double calcSunEqOfCenter(double T)
 {
     double M = calcGeomMeanAnomalySun(T);
-    return sin(radians(M)) * 1.914602 + sin(2 * radians(M)) * 0.019993;  // in degrees
+    return sin(radians(M)) * (1.914602 - 0.004817 * T) +
+           sin(2 * radians(M)) * 0.019993; /* in degrees */
 }
 
 double calcSunRadVector(double T)
 {
     double M = calcGeomMeanAnomalySun(T);
-    return 1.00014 - 0.01671 * cos(radians(M)) - 0.00014 * cos(2 * radians(M));  // in AUs
+    return 1.00014 - 0.01671 * cos(radians(M)) -
+           0.00014 * cos(2 * radians(M)); /* in AUs */
 }
 
 double calcMeanObliquityOfEcliptic(double T)
 {
-    return 23.4392911 - T * 0.0130042;  // in degrees
+    return 23.4392911 - T * 0.0130042; /* in degrees */
 }
 
-// Mean geocentric equatorial coordinates, accurate to ~1 arcminute
-void calcSolarCoordinates(double T, double *ra, double *dec)
+/* Mean geocentric equatorial coordinates, accurate to ~1 arcminute */
+void calcSolarCoordinates(double T,
+                          double *ra, double *dec)
 {
     double L0 = calcGeomMeanLongSun(T);
     double C = calcSunEqOfCenter(T);
-    double L = L0 + C - 0.00569;  // corrected for aberration
+    double L = L0 + C - 0.00569; /* corrected for aberration */
 
     double eps = calcMeanObliquityOfEcliptic(T);
-    *ra = degrees(atan2(cos(radians(eps)) * sin(radians(L)), cos(radians(L))));  // [-180, 180)
+    *ra = degrees(atan2(cos(radians(eps)) * sin(radians(L)), cos(radians(L))));
     *dec = degrees(asin(sin(radians(eps)) * sin(radians(L))));
 }
 
-double calcGrMeanSiderealTime(struct JulianDay jd)
+double calcGrMeanSiderealTime(JulianDay jd)
 {
     double GMST = wrapTo360(100.46061837 + 0.98564736629 * (jd.JD - 2451545));
-    return wrapTo360(GMST + 360.985647 * jd.m);  // in degrees
+    return wrapTo360(GMST + 360.985647 * jd.m); /* in degrees */
 }
 
-void equatorial2horizontal(double H, double dec, double lat, double *az, double *el)
+void equatorial2horizontal(double H, double dec, double lat,
+                           double *az, double *el)
 {
-    *az = degrees(atan2(sin(radians(H)), cos(radians(H)) * sin(radians(lat)) - tan(radians(dec)) * cos(radians(lat))));
-    *el = degrees(asin(sin(radians(lat)) * sin(radians(dec)) + cos(radians(lat)) * cos(radians(dec)) * cos(radians(H))));
+    double xhor = cos(radians(H)) * cos(radians(dec)) * sin(radians(lat)) -
+                  sin(radians(dec)) * cos(radians(lat));
+    double yhor = sin(radians(H)) * cos(radians(dec));
+    double zhor = cos(radians(H)) * cos(radians(dec)) * cos(radians(lat)) +
+                  sin(radians(dec)) * sin(radians(lat));
+
+    *az = degrees(atan2(yhor, xhor));
+    *el = degrees(atan2(zhor, sqrt(xhor * xhor + yhor * yhor)));
 }
 
-// Approximate atmospheric refraction correction, in degrees
+/* Hour angle at sunrise or sunset, returns NaN if circumpolar */
+double calcHourAngleRiseSet(double dec, double lat, double h0)
+{
+    return degrees(acos((sin(radians(h0)) -
+                         sin(radians(lat)) * sin(radians(dec))) /
+                        (cos(radians(lat)) * cos(radians(dec)))));
+}
+
+/* Approximate atmospheric refraction correction, in degrees */
 double calcRefraction(double elev)
 {
     if (elev < -0.575)
-        return -20.774 / tan(radians(elev)) / 3600;  // Zimmerman (1981)
+        /* (Zimmerman, 1981) */
+        return -20.774 / tan(radians(elev)) / 3600;
     else
-        return 1.02 / tan(radians(elev + 10.3 / (elev + 5.11))) / 60;  // Sæmundsson (1986)
+        /* (Sæmundsson, 1986) */
+        return 1.02 / tan(radians(elev + 10.3 / (elev + 5.11))) / 60;
 }
 
-//======================================================================================================================
-// Solar calculator
-//
-// All calculations assume time inputs in Coordinated Universal Time (UTC)
-//======================================================================================================================
+/*
+ * Solar calculator
+ */
 
-// Equation of time, in minutes of time
-void calcEquationOfTime(struct JulianDay jd, double *E)
+/* Equation of time, in minutes of time */
+void calcEquationOfTime(JulianDay jd,
+                        double *E)
 {
     double T = calcJulianCent(jd);
     double L0 = calcGeomMeanLongSun(T);
@@ -161,8 +185,10 @@ void calcEquationOfTime(struct JulianDay jd, double *E)
     *E = 4 * wrapTo180(L0 - 0.00569 - ra);
 }
 
-// Sun's geocentric (as seen from the center of the Earth) equatorial coordinates, in degrees and AUs
-void calcEquatorialCoordinates(struct JulianDay jd, double *rt_ascension, double *declination, double *radius_vector)
+/* Sun's (geocentric) equatorial coordinates, in degrees and AUs */
+void calcEquatorialCoordinates(JulianDay jd,
+                               double *rt_ascension, double *declination,
+                               double *radius_vector)
 {
     double T = calcJulianCent(jd);
     calcSolarCoordinates(T, rt_ascension, declination);
@@ -171,8 +197,8 @@ void calcEquatorialCoordinates(struct JulianDay jd, double *rt_ascension, double
     *radius_vector = calcSunRadVector(T);
 }
 
-// Sun's topocentric (as seen from the observer's place on the Earth's surface) horizontal coordinates, in degrees
-void calcHorizontalCoordinates(struct JulianDay jd, double latitude, double longitude,
+/* Sun's (topocentric) horizontal coordinates, in degrees */
+void calcHorizontalCoordinates(JulianDay jd, double latitude, double longitude,
                                double *azimuth, double *elevation)
 {
     double T = calcJulianCent(jd);
@@ -184,154 +210,39 @@ void calcHorizontalCoordinates(struct JulianDay jd, double latitude, double long
     double H = GMST + longitude - ra;
     equatorial2horizontal(H, dec, latitude, azimuth, elevation);
 
-    *azimuth += 180;  // measured from the North
+    *azimuth += 180; /* measured from the North */
     *elevation += calcRefraction(*elevation);
 }
 
-// Helper function
-void calcRiseSetTimes(double m[3], struct JulianDay jd, double latitude, double longitude, double h0)
+/* Find the times of sunrise, transit, and sunset, in hours */
+void calcSunriseSunset(JulianDay jd, double latitude, double longitude,
+                       double *transit, double *sunrise, double *sunset,
+                       double altitude, int iterations)
 {
-    double T = calcJulianCent(jd);
-    double GMST = calcGrMeanSiderealTime(jd);
-
-    double ra, dec;
-    calcSolarCoordinates(T, &ra, &dec);
-
-    // Local hour angle at sunrise or sunset (±NaN if body is circumpolar)
-    double H0 = degrees(acos((sin(radians(h0)) - sin(radians(latitude)) * sin(radians(dec))) /
-                        (cos(radians(latitude)) * cos(radians(dec)))));
-
-    m[0] = jd.m + wrapTo180(ra - longitude - GMST) / 360;
-    m[1] = m[0] - H0 / 360;
-    m[2] = m[0] + H0 / 360;
-}
-
-// Find the times of sunrise, transit, and sunset, in hours
-void calcSunriseSunset(struct JulianDay jd, double latitude, double longitude,
-                       double *transit, double *sunrise, double *sunset, double altitude, int iterations)
-{
-    double m[3], times[3];
+    double m[3];
     m[0] = 0.5 - longitude / 360;
 
-    for (int i = 0; i <= iterations; i++)
-        for (int event = 0; event < 3; event++)
+    int i, event;
+    for (i = 0; i <= iterations; i++)
+        for (event = 0; event < 3; event++)
         {
             jd.m = m[event];
-            calcRiseSetTimes(times, jd, latitude, longitude, altitude);
-            m[event] = times[event];
+            double T = calcJulianCent(jd);
+            double GMST = calcGrMeanSiderealTime(jd);
 
-            // First iteration, approximate rise and set times
-            if (i == 0)
-            {
-                m[1] = times[1];
-                m[2] = times[2];
-                break;
-            }
+            double ra, dec;
+            calcSolarCoordinates(T, &ra, &dec);
+
+            double m0 = jd.m + wrapTo180(ra - longitude - GMST) / 360;
+            double d0 = calcHourAngleRiseSet(dec, latitude, altitude) / 360;
+
+            if (event == 0) m[0] = m0;
+            if (event == 1 || i == 0) m[1] = m0 - d0;
+            if (event == 2 || i == 0) m[2] = m0 + d0;
+            if (i == 0) break;
         }
 
     *transit = m[0] * 24;
     *sunrise = m[1] * 24;
     *sunset = m[2] * 24;
-}
-
-//======================================================================================================================
-// Wrapper functions
-//
-// All calculations assume time inputs in Coordinated Universal Time (UTC)
-//======================================================================================================================
-
-void calcEquationOfTimeFromUnix(unsigned long utc, double *E)
-{
-    struct JulianDay jd = jdFromUnix(utc);
-    calcEquationOfTime(jd, E);
-}
-
-void calcEquationOfTimeFromDate(int year, int month, int day, int hour, int minute, int second, double *E)
-{
-    struct JulianDay jd = jdFromDate(year, month, day, hour, minute, second);
-    calcEquationOfTime(jd, E);
-}
-
-void calcEquatorialCoordinatesFromUnix(unsigned long utc,
-                                       double *rt_ascension, double *declination, double *radius_vector)
-{
-    struct JulianDay jd = jdFromUnix(utc);
-    calcEquatorialCoordinates(jd, rt_ascension, declination, radius_vector);
-}
-
-void calcEquatorialCoordinatesFromDate(int year, int month, int day, int hour, int minute, int second,
-                                       double *rt_ascension, double *declination, double *radius_vector)
-{
-    struct JulianDay jd = jdFromDate(year, month, day, hour, minute, second);
-    calcEquatorialCoordinates(jd, rt_ascension, declination, radius_vector);
-}
-
-void calcHorizontalCoordinatesFromUnix(unsigned long utc, double latitude, double longitude,
-                                       double *azimuth, double *elevation)
-{
-    struct JulianDay jd = jdFromUnix(utc);
-    calcHorizontalCoordinates(jd, latitude, longitude, azimuth, elevation);
-}
-
-void calcHorizontalCoordinatesFromDate(int year, int month, int day, int hour, int minute, int second,
-                                       double latitude, double longitude, double *azimuth, double *elevation)
-{
-    struct JulianDay jd = jdFromDate(year, month, day, hour, minute, second);
-    calcHorizontalCoordinates(jd, latitude, longitude, azimuth, elevation);
-}
-
-void calcSunriseSunsetFromUnix(unsigned long utc, double latitude, double longitude,
-                               double *transit, double *sunrise, double *sunset)
-{
-    struct JulianDay jd = jdFromUnix(utc);
-    calcSunriseSunset(jd, latitude, longitude, transit, sunrise, sunset, SUNRISESET_STD_ALTITUDE, 1);
-}
-
-void calcSunriseSunsetFromDate(int year, int month, int day, double latitude, double longitude,
-                               double *transit, double *sunrise, double *sunset)
-{
-    struct JulianDay jd = jdFromDate(year, month, day, 0, 0, 0);
-    calcSunriseSunset(jd, latitude, longitude, transit, sunrise, sunset, SUNRISESET_STD_ALTITUDE, 1);
-}
-
-void calcCivilDawnDuskFromUnix(unsigned long utc, double latitude, double longitude,
-                               double *transit, double *dawn, double *dusk)
-{
-    struct JulianDay jd = jdFromUnix(utc);
-    calcSunriseSunset(jd, latitude, longitude, transit, dawn, dusk, CIVIL_DAWNDUSK_STD_ALTITUDE, 1);
-}
-
-void calcCivilDawnDuskFromDate(int year, int month, int day, double latitude, double longitude,
-                               double *transit, double *dawn, double *dusk)
-{
-    struct JulianDay jd = jdFromDate(year, month, day, 0, 0, 0);
-    calcSunriseSunset(jd, latitude, longitude, transit, dawn, dusk, CIVIL_DAWNDUSK_STD_ALTITUDE, 1);
-}
-
-void calcNauticalDawnDuskFromUnix(unsigned long utc, double latitude, double longitude,
-                                  double *transit, double *dawn, double *dusk)
-{
-    struct JulianDay jd = jdFromUnix(utc);
-    calcSunriseSunset(jd, latitude, longitude, transit, dawn, dusk, NAUTICAL_DAWNDUSK_STD_ALTITUDE, 1);
-}
-
-void calcNauticalDawnDuskFromDate(int year, int month, int day, double latitude, double longitude,
-                                  double *transit, double *dawn, double *dusk)
-{
-    struct JulianDay jd = jdFromDate(year, month, day, 0, 0, 0);
-    calcSunriseSunset(jd, latitude, longitude, transit, dawn, dusk, NAUTICAL_DAWNDUSK_STD_ALTITUDE, 1);
-}
-
-void calcAstronomicalDawnDuskFromUnix(unsigned long utc, double latitude, double longitude,
-                                      double *transit, double *dawn, double *dusk)
-{
-    struct JulianDay jd = jdFromUnix(utc);
-    calcSunriseSunset(jd, latitude, longitude, transit, dawn, dusk, ASTRONOMICAL_DAWNDUSK_STD_ALTITUDE, 1);
-}
-
-void calcAstronomicalDawnDuskFromDate(int year, int month, int day, double latitude, double longitude,
-                                      double *transit, double *dawn, double *dusk)
-{
-    struct JulianDay jd = jdFromDate(year, month, day, 0, 0, 0);
-    calcSunriseSunset(jd, latitude, longitude, transit, dawn, dusk, ASTRONOMICAL_DAWNDUSK_STD_ALTITUDE, 1);
 }
--- CSolarCalculator.h
+++ CSolarCalculator.h
@@ -1,11 +1,13 @@
-//======================================================================================================================
-// SolarCalculator Library for Arduino (C version)
-//
-// This library provides functions to calculate the times of sunrise, sunset, solar noon, twilight (dawn and dusk),
-// Sun's apparent position in the sky, equation of time, etc.
-//
-// Most formulae are taken from Astronomical Algorithms by Jean Meeus and optimized for 8-bit AVR platform.
-//======================================================================================================================
+/*
+ * SolarCalculator Library for Arduino (C version)
+ *
+ * This library provides functions to calculate the times of sunrise, sunset,
+ * solar noon, twilight (dawn and dusk), Sun's apparent position in the sky,
+ * equation of time, etc.
+ *
+ * Most formulae are taken from Astronomical Algorithms by Jean Meeus and
+ * optimized for Arduino.
+ */
 
 #ifndef CSOLARCALCULATOR_H
 #define CSOLARCALCULATOR_H
@@ -14,112 +16,94 @@
 extern "C" {
 #endif
 
-extern const double SUNRISESET_STD_ALTITUDE;
-extern const double CIVIL_DAWNDUSK_STD_ALTITUDE;
-extern const double NAUTICAL_DAWNDUSK_STD_ALTITUDE;
-extern const double ASTRONOMICAL_DAWNDUSK_STD_ALTITUDE;
+#define SUNRISESET_ALTITUDE (-0.8333)
+#define CIVIL_DAWNDUSK_ALTITUDE (-6.0)
+#define NAUTICAL_DAWNDUSK_ALTITUDE (-12.0)
+#define ASTRONOMICAL_DAWNDUSK_ALTITUDE (-18.0)
 
 struct JulianDay
 {
-    double JD;  // Julian day at 0h UT (JD ending in .5)
-    double m;   // Fractional day, 0h to 24h (decimal number between 0 and 1)
+    double JD; /* Julian day at 0h UT (JD ending in .5) */
+    double m;  /* fractional day (decimal number between 0 and 1) */
 };
 
-struct JulianDay jdFromUnix(unsigned long utc);  // Unix time, i.e. seconds since 0h UT 1 January 1970
-struct JulianDay jdFromDate(int year, int month, int day, int hour, int minute, int second);  // Calendar date (UTC)
+typedef struct JulianDay JulianDay;
 
-//======================================================================================================================
-// Intermediate calculations
-//
-// Time T is measured in Julian centuries (36525 ephemeris days from the epoch J2000.0)
-//======================================================================================================================
+/*
+ * Get Julian Day, given:
+ *
+ *  - Unix timestamp, elapsed seconds since 1970 Jan 01, 00:00:00 UTC, e.g.
+ *      ts      946684800       2000 Jan 01, 00:00:00 UTC
+ *
+ *  - Calendar date and time (UTC), with valid ranges:
+ *      year    [1901..2099]
+ *      month   [1..12]
+ *      day     [1..31]
+ *      hour    [0..23]
+ *      minute  [0..59]
+ *      second  [0..60]
+ */
 
-// Utilities
+JulianDay jdFromUnix(unsigned long unix_ts);
+JulianDay jdFromDate(int year, int month, int day,
+                     int hour, int minute, int second);
+
+/*
+ * Intermediate calculations
+ *
+ * NB. Time T is measured in Julian centuries
+ *     (36525 ephemeris days from the epoch J2000.0)
+ */
+
+/* Utilities */
 double wrapTo360(double angle);
 double wrapTo180(double angle);
 
-// Julian day
-double fractionalDay(int hour, int minute, int second);
-double calcJulianDay(int year, int month, int day);
-double calcJulianCent(struct JulianDay jd);
+/* Julian centuries */
+double calcJulianCent(JulianDay jd);
 
-// Solar coordinates
+/* Solar coordinates */
 double calcGeomMeanLongSun(double T);
 double calcGeomMeanAnomalySun(double T);
 double calcSunEqOfCenter(double T);
 double calcSunRadVector(double T);
 double calcMeanObliquityOfEcliptic(double T);
-void calcSolarCoordinates(double T, double *ra, double *dec);
+void calcSolarCoordinates(double T,
+                          double *ra, double *dec);
 
-// Sidereal time at Greenwich
-double calcGrMeanSiderealTime(struct JulianDay jd);
+/* Sidereal time at Greenwich */
+double calcGrMeanSiderealTime(JulianDay jd);
 
-// Sun's position in the sky
-void equatorial2horizontal(double H, double dec, double lat, double *az, double *el);
+/* Sun's position in the sky */
+void equatorial2horizontal(double H, double dec, double lat,
+                           double *az, double *el);
+double calcHourAngleRiseSet(double dec, double lat, double h0);
 double calcRefraction(double elev);
 
-//======================================================================================================================
-// Solar calculator
-//
-// All calculations assume time inputs in Coordinated Universal Time (UTC)
-//======================================================================================================================
+/*
+ * Solar calculator
+ */
 
-// Equation of time, in minutes of time
-void calcEquationOfTime(struct JulianDay jd, double *E);
+/* Equation of time, in minutes of time */
+void calcEquationOfTime(JulianDay jd,
+                        double *E);
 
-// Sun's geocentric (as seen from the center of the Earth) equatorial coordinates, in degrees and AUs
-void calcEquatorialCoordinates(struct JulianDay jd, double *rt_ascension, double *declination, double *radius_vector);
+/* Sun's (geocentric) equatorial coordinates, in degrees and AUs */
+void calcEquatorialCoordinates(JulianDay jd,
+                               double *rt_ascension, double *declination,
+                               double *radius_vector);
 
-// Sun's topocentric (as seen from the observer's place on the Earth's surface) horizontal coordinates, in degrees
-void calcHorizontalCoordinates(struct JulianDay jd, double latitude, double longitude,
+/* Sun's (topocentric) horizontal coordinates, in degrees */
+void calcHorizontalCoordinates(JulianDay jd, double latitude, double longitude,
                                double *azimuth, double *elevation);
 
-// Find the times of sunrise, transit, and sunset, in hours
-void calcSunriseSunset(struct JulianDay jd, double latitude, double longitude,
+/* Find the times of sunrise, transit, and sunset, in hours */
+void calcSunriseSunset(JulianDay jd, double latitude, double longitude,
                        double *transit, double *sunrise, double *sunset,
                        double altitude, int iterations);
-
-//======================================================================================================================
-// Wrapper functions
-//
-// All calculations assume time inputs in Coordinated Universal Time (UTC)
-//======================================================================================================================
-
-void calcEquationOfTimeFromUnix(unsigned long utc, double *E);
-void calcEquationOfTimeFromDate(int year, int month, int day, int hour, int minute, int second, double *E);
-
-void calcEquatorialCoordinatesFromUnix(unsigned long utc,
-                                       double *rt_ascension, double *declination, double *radius_vector);
-void calcEquatorialCoordinatesFromDate(int year, int month, int day, int hour, int minute, int second,
-                                       double *rt_ascension, double *declination, double *radius_vector);
-
-void calcHorizontalCoordinatesFromUnix(unsigned long utc, double latitude, double longitude,
-                                       double *azimuth, double *elevation);
-void calcHorizontalCoordinatesFromDate(int year, int month, int day, int hour, int minute, int second,
-                                       double latitude, double longitude, double *azimuth, double *elevation);
-
-void calcSunriseSunsetFromUnix(unsigned long utc, double latitude, double longitude,
-                               double *transit, double *sunrise, double *sunset);
-void calcSunriseSunsetFromDate(int year, int month, int day, double latitude, double longitude,
-                               double *transit, double *sunrise, double *sunset);
-
-void calcCivilDawnDuskFromUnix(unsigned long utc, double latitude, double longitude,
-                               double *transit, double *dawn, double *dusk);
-void calcCivilDawnDuskFromDate(int year, int month, int day, double latitude, double longitude,
-                               double *transit, double *dawn, double *dusk);
-
-void calcNauticalDawnDuskFromUnix(unsigned long utc, double latitude, double longitude,
-                                  double *transit, double *dawn, double *dusk);
-void calcNauticalDawnDuskFromDate(int year, int month, int day, double latitude, double longitude,
-                                  double *transit, double *dawn, double *dusk);
-
-void calcAstronomicalDawnDuskFromUnix(unsigned long utc, double latitude, double longitude,
-                                      double *transit, double *dawn, double *dusk);
-void calcAstronomicalDawnDuskFromDate(int year, int month, int day, double latitude, double longitude,
-                                      double *transit, double *dawn, double *dusk);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif  //CSOLARCALCULATOR_H
+#endif /* CSOLARCALCULATOR_H */
